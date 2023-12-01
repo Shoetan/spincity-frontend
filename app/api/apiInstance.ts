665af6aefcf922e2ecfc605065a25f59118fbb47
Shoetan <emmysoetan@gmail.com>
2023-12-01 07:53:38 +0100

    feat: added react query to fetch data from endpoint

--- app/api/apiInstance.ts
+++ app/api/apiInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const instacnce = axios.create ({
+const instance = axios.create ({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL
 })
 
-export default instacnce
+export default instance
